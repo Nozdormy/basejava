e24b8cdcd0ae1785d53617e40fe69fe363a99c1a
Sergey <goncharov96snz@gmail.com>
2023-01-19 18:57:03 +0500

    HW_1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,54 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size = 0;
 
     void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
     }
 
     void save(Resume r) {
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
-        return null;
+        int num = findNum(uuid);
+        return num != -1 ? storage[num] : null;
     }
 
     void delete(String uuid) {
+        int num = findNum(uuid);
+        if (num != -1) {
+            System.arraycopy(storage, num + 1, storage, num, size - (num + 1));
+            size--;
+            return;
+        }
+        System.out.println("Не удалено");
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
-        return 0;
+        return size;
+    }
+
+    private int findNum(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
